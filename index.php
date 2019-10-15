e551d3d97a25f597c8e3de76858f05c6784f9532
Andrés Saam <andressaa.dev@gmail.com>
2019-10-15 16:19:03 -0500

    Primmer commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$hola = "Hola Mundo";
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Repaso PHP</title>
+</head>
+
+<body>
+    <div>
+        <h1>Andrés Saa <?php echo $hola ?></h1>
+        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum incidunt ipsam ullam est ipsum autem.</h3>
+    </div>
+    <div>
+        <h2>Skills and Roles</h2>
+        <h3>WebView Developer</h3>
+        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, quod sint est accusantium placeat nisi repellendus asperiores modi quibusdam ratione, molestias quidem libero velit, voluptas totam ullam dignissimos nulla unde.</h4>
+        <h3>React Developer</h3>
+        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, quod sint est accusantium placeat nisi repellendus asperiores modi quibusdam ratione, molestias quidem libero velit, voluptas totam ullam dignissimos nulla unde.</h4>
+        <h3>Frontend Developer</h3>
+        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, quod sint est accusantium placeat nisi repellendus asperiores modi quibusdam ratione, molestias quidem libero velit, voluptas totam ullam dignissimos nulla unde.</h4>
+    </div>
+</body>
+
+</html>
