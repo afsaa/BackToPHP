d9211e09112931fda84539e475e9d8c59c0c0b87
Andrés Saam <andressaa.dev@gmail.com>
2019-10-24 15:44:41 -0500

    Instalando Eloquent ORM para poder usar  multiples manejadores de DB (SQLite, MYSQL, Postgresql, etc)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit5bbe238095be88d416a10c363e8aa284
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5bbe238095be88d416a10c363e8aa284::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5bbe238095be88d416a10c363e8aa284($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5bbe238095be88d416a10c363e8aa284($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
