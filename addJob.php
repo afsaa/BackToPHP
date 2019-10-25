5eecc6c323ef8dcb8dacd4afac52b76500ed183d
Andrés Saam <andressaa.dev@gmail.com>
2019-10-25 17:28:59 -0500

    Empezando a usar routing para usar el código de Eloquent solo desde public/index.php

--- addJob.php
+++ addJob.php
@@ -1,28 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-use Illuminate\Database\Capsule\Manager as Capsule;
 use App\Models\Job;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'curso-php',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
 
 if (!empty($_POST)) {
     $job = new Job();
@@ -43,6 +21,7 @@ if (!empty($_POST)) {
 </head>
 
 <body>
+    <h1>Add Job</h1>
     <form action="addJob.php" method="post">
         <label for="title">Title: </label>
         <input type="text" name="title">
--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
 require "jobs.php";
 require "projects.php";
-require_once 'vendor/autoload.php';
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'curso-php',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
 ?>
 <!DOCTYPE html>
 <html lang="es">
